57430fe091d6b0df91756c3385c8134ee319198a
Aylen2005 <93220078+Aylen2005@users.noreply.github.com>
2024-09-03 14:35:03 -0300

    14:34hs

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,17 @@ function App() {
   const [ciudad, setCiudad] = useState("Cordoba");
 
   const apiKey = 'c7cb38a58f7e2584b62dc90338925cbe';
+  const apikey2 = '77a3bb6ad3714fb1a127c0673d23b946'
 
   useEffect(() => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`;
-
+    const url2 = `https://api.opencagedata.com/geocode/v1/json?key=${apikey2}&q=52.3877830%2C+9.7334394&pretty=1&no_annotations=1`
     axios.get(url)
       .then(response => {
         const datos = response.data;
-        const { latitud, longitud } = datos.coord;
+        const { latitud , longitud } = datos.coord;
         setClima(datos);
+
         return axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${latitud}&lon=${longitud}&appid=${apiKey}`);
       })
       .then(response => {
@@ -31,35 +33,65 @@ function App() {
   const buscarLugar = (event) => {
     setLugar(event.target.value);
   }
+
+
+
   return (
     <>
+      <nav>
+        <img src="" alt="" />
+        <ul className='nav__list'>
+          <li className='nav__item'><a className='nav__link'href="#">Contaminacion</a></li>
+          <li className='nav__item'><a className='nav__link'href="#">Sobre Nosotros</a></li>
+        </ul>
+      </nav>
       <h1>Bienvenido a Climator</h1>
 
       <input 
         type="text" 
-        placeholder="pone la ciudad" 
+        id= "BoxText"
+        placeholder="Ingresa una ciudad" 
         value={lugar} 
-        onChange={buscarLugar}/>
+        onChange={buscarLugar} 
+      />
 
       <button onClick={() => setCiudad(lugar)}>Buscar</button>
+
       <div>
         {clima && (
-          <div>
+          <div id = "hijo">
             <h2>Clima en {clima.name}</h2>
             <p>Temperatura: {clima.main.temp}°C</p>
             <p>Humedad: {clima.main.humidity}%</p>
           </div>
         )}
       </div>
-
       <div>
-        {aire && (
-          <div>
-            <h2>Calidad del aire</h2>
-            <p>Índice de calidad: {aire.list[0].main.aqi}</p>
+        {aire && aire.list && aire.list.length > 0 && (
+          <div id = "hijo">
+            <h2>calidad del aire</h2>
+            <p>indice: {aire.list[0].main.aqi}</p>
+            <h3>concentracion de contaminantes:</h3>
+            <p>PM2.5: {aire.list[0].components.pm2_5} µg/m³</p>
+            <p>PM10: {aire.list[0].components.pm10} µg/m³</p>
+            <p>O₃: {aire.list[0].components.o3} µg/m³</p>
+            <p>NO₂: {aire.list[0].components.no2} µg/m³</p>
+            <p>SO₂: {aire.list[0].components.so2} µg/m³</p>
+            <p>CO: {aire.list[0].components.co} µg/m³</p>
           </div>
         )}
       </div>
+      <div>
+        {clima && (
+          <div id = "hijo">
+            <h2>Clima en {clima.name}</h2>
+            <p>Temperatura: {clima.main.temp}°C</p>
+            <p>Humedad: {clima.main.humidity}%</p>
+            <p>Velocidad del viento: {clima.wind.speed} m/s</p>
+          </div>
+        )}
+      </div>
+
     </>
   );
 }
