5b90c78c5af2819de21ef82e1f2cc0bd8620a90a
Aylen2005 <93220078+Aylen2005@users.noreply.github.com>
2024-09-03 12:32:15 -0300

    Co-authored-by: Fran <FranMaiolo@users.noreply.github.com> Co-authored-by: thiago2307 <thiago2307@users.noreply.github.com> Co-authored-by: humildegit@gmail.com <humildegit@gmail.com> Co-authored-by: ezequiel_pc <estebanpc1918@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [lugar, setLugar] = useState("");
+  const [clima, setClima] = useState("");
+  const [aire, setAire] = useState("");
+  const [ciudad, setCiudad] = useState("Cordoba");
 
+  const apiKey = 'c7cb38a58f7e2584b62dc90338925cbe';
+
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`;
+
+    axios.get(url)
+      .then(response => {
+        const datos = response.data;
+        const { latitud, longitud } = datos.coord;
+        setClima(datos);
+        return axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${latitud}&lon=${longitud}&appid=${apiKey}`);
+      })
+      .then(response => {
+        setAire(response.data);
+      })
+      .catch(error => {
+        console.error('Error al hacer la solicitud:', error);
+      });
+  }, [ciudad]);
+
+  const buscarLugar = (event) => {
+    setLugar(event.target.value);
+  }
   return (
     <>
+      <h1>Bienvenido a Climator</h1>
+
+      <input 
+        type="text" 
+        placeholder="Ingresa una ciudad" 
+        value={lugar} 
+        onChange={buscarLugar}/>
+
+      <button onClick={() => setCiudad(lugar)}>Buscar</button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {clima && (
+          <div>
+            <h2>Clima en {clima.name}</h2>
+            <p>Temperatura: {clima.main.temp}°C</p>
+            <p>Humedad: {clima.main.humidity}%</p>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        {aire && (
+          <div>
+            <h2>Calidad del aire</h2>
+            <p>Índice de calidad: {aire.list[0].main.aqi}</p>
+          </div>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
